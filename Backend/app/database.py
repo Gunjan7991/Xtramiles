9ce28d59952ffd2ba8dd41c36557f3652fcdc7cb
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2025-02-05 08:21:33 -0600

    Cleanup
    Cleanup and dynamic url for database. logger implemented instead of printing

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -2,22 +2,18 @@ from sqlmodel import  create_engine, SQLModel, Session
 from typing import Annotated
 from fastapi import Depends
 
-# SQL LITE
-# sqlite_file_name = "database.db"
-# sqlite_url = f"sqlite:///{sqlite_file_name}"
-# SQLALCHEMY_DATABASE_URL = sqlite_url
+from .config import DATABASE_URL
+from .logging_config import logger
 
-# PostgreSQL
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:apple@localhost:5432/xtraMiles"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo = False)
+engine = create_engine(DATABASE_URL)
 
 
 def init_db():
     try:
         SQLModel.metadata.create_all(engine)
-        print("Database initialized successfully.")
+        logger.info("DATABASE INITIALIZED SUCESSFULLY.")
     except Exception as e:
-        print(f"Error initializing database: {e}")
+        logger.fatal(f"Failed to initialize database!{e}")
 
 
 def get_session():
