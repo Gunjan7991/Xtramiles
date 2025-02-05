fbd200a6d50f128abfc34c7791c285e66b5d264b
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2025-02-05 08:22:14 -0600

    update name of file
    updates name of the file, adds date on the end of fine name

--- Backend/app/logging_config.py
+++ Backend/app/logging_config.py
@@ -1,17 +1,18 @@
 import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
+import datetime
 
 # Define log directory as "log"
 LOG_DIR = "logs"
-
+date = (str(datetime.datetime.now()).split(" ")[0]).replace("-", "_")
 # Ensure log directory exists
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
 # Create Timed Rotating File Handler (New log file every day)
 log_handler = TimedRotatingFileHandler(
-    os.path.join(LOG_DIR, "app.log"), when="midnight", interval=1, backupCount=30
+    os.path.join(LOG_DIR, f"app_{date}.log"), when="midnight", interval=1, backupCount=30
 )
 log_handler.suffix = "%Y-%m-%d"  # File format: app.log_YYYY-MM-DD
 log_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
@@ -21,3 +22,4 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 # logger.addHandler(logging.StreamHandler())  # Also log to console
+
